85b562c87c56de2393511e1db6d354a42a8d1bab
farhanT23 <farhan.tanvir@brainstation-23.com>
2025-03-24 10:47:44 +0600

    Added Factorial Calculation

---
+++ basic_program.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int factorial(int n) {
+    return (n == 0) ? 1 : n * factorial(n - 1);
+}
+
+int main() {
+    int num = 5;
+    printf("Factorial of %d is %d\n", num, factorial(num));
+    return 0;
+}
