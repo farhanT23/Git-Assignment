a2f5859ae45bdd2f7094c707cbcc0098ab35e765
farhanT23 <farhan.tanvir@brainstation-23.com>
2025-03-24 10:59:27 +0600

    Added Fibonacci Function

--- basic_program.cpp
+++ basic_program.cpp
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+int fibonacci(int n) {
+    if (n <= 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 int factorial(int n) {
     return (n == 0) ? 1 : n * factorial(n - 1);
 }
